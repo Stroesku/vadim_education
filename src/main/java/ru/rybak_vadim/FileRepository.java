d6c5077d13ea1c9e2216a6bd7abac3810d21ff9c
Vadim Rybak <vadim2006rybak@gmail.com>
2024-04-25 20:20:39 +0300

    чтение списка из файла

--- src/main/java/ru/rybak_vadim/FileRepository.java
+++ src/main/java/ru/rybak_vadim/FileRepository.java
@@ -62,36 +62,6 @@ public class FileRepository {
                 e.printStackTrace();
             }
         }
-
-
         return arrayList;
     }
-    public int [] sort(int array) {
-        int [] listOfNumbers = new int[0];
-        boolean sorted = false; //создание переменной sorted и присваивание ей значения false.
-//        создаем цикл while, который выполняется пока sorted не стане равной true
-        while (!sorted) {
-//            присваивание переменной sorted значения true. если массив уже отсортирован то цикл закончится
-            sorted = true;
-//            перебираем элементы массива
-            for (int s = 1; s < listOfNumbers.length; s++) {
-                // if проверяет если первое значение текущей позиции меньше предыдущей
-                if (listOfNumbers[s] < listOfNumbers[s - 1]) {
-//                   создание временной переменной для текущего значения массива
-                    int temp = listOfNumbers[s];
-                    // замена текущего значения на предыдущее
-                    listOfNumbers[s] = listOfNumbers[s - 1];
-//                    замена предыдущего значения с сохраненым временым значением
-                    listOfNumbers[s - 1] = temp;
-                    sorted = false;
-                }
-            }
-
-        }
-        return listOfNumbers;
-    }
-
-
-
-
 }
--- src/main/java/ru/rybak_vadim/IntegerGenerator.java
+++ src/main/java/ru/rybak_vadim/IntegerGenerator.java
@@ -10,7 +10,6 @@ public class IntegerGenerator {
         return randomNumber;
     }
 
-
     public int[] generateNumbers(int numberOfElements, int min, int max) {
         int[] generatedNumbers = new int[numberOfElements];
         for (int i = 0; i < numberOfElements; i++) {
@@ -19,5 +18,27 @@ public class IntegerGenerator {
         return generatedNumbers;
     }
 
-
+    public int[] sort(int array) {
+        int[] listOfNumbers = new int[0];
+        boolean sorted = false; //создание переменной sorted и присваивание ей значения false.
+//        создаем цикл while, который выполняется пока sorted не стане равной true
+        while (!sorted) {
+//            присваивание переменной sorted значения true. если массив уже отсортирован то цикл закончится
+            sorted = true;
+//            перебираем элементы массива
+            for (int s = 1; s < listOfNumbers.length; s++) {
+                // if проверяет если первое значение текущей позиции меньше предыдущей
+                if (listOfNumbers[s] < listOfNumbers[s - 1]) {
+//                   создание временной переменной для текущего значения массива
+                    int temp = listOfNumbers[s];
+                    // замена текущего значения на предыдущее
+                    listOfNumbers[s] = listOfNumbers[s - 1];
+//                    замена предыдущего значения с сохраненым временым значением
+                    listOfNumbers[s - 1] = temp;
+                    sorted = false;
+                }
+            }
+        }
+        return listOfNumbers;
+    }
 }
