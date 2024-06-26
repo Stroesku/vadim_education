0e1541e894873c2b8b0315881e66e2aa0af0f06a
Vadim Rybak <vadim2006rybak@gmail.com>
2024-06-26 13:43:50 +0300

    add enum class and removing the bag

--- src/main/java/ru/rybak_vadim/Main.java
+++ src/main/java/ru/rybak_vadim/Main.java
@@ -1,5 +1,6 @@
 package ru.rybak_vadim;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,25 +9,41 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    public enum Option{
+        EXISTINGLIST,
+        ADDNEWLIST
+    }
+
     public static void main(String[] args) {
         IntegerGenerator integerGenerator = new IntegerGenerator();
         FileRepository fileRepository = new FileRepository();
         ArrayList<Integer> list = fileRepository.getArray();
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("1) Показать существующие списки ");
         System.out.println("2)добавить новый список ");
-        int choise = scanner.nextInt();
 
-        if (choise == 1) {
-            checkingScenario(list);
+        Option choise = null;
+        int input = scanner.nextInt();
+        if (input == 1) {
+            choise = Option.EXISTINGLIST;
+        } else if (input == 2) {
+            choise = Option.ADDNEWLIST;
         }
-        if (choise == 2) {
-            generateListScenario(integerGenerator, fileRepository, scanner);
-
-
+        else{
+            System.out.println("выберите возможный вариант ответа");
+        }
+        switch (choise) {
+            case EXISTINGLIST -> {
+                checkingScenario(list);
+                break;
+            }
+            case ADDNEWLIST -> {
+                generateListScenario(integerGenerator, fileRepository, scanner);
+                break;
+            }
         }
     }
-
     public static void checkingScenario(ArrayList<Integer> list) {
         if (list.isEmpty()) {
             System.out.println("Список пуст");
@@ -42,15 +59,16 @@ public class Main {
         int min = scanner.nextInt();
         System.out.println("Введите максимальное число: ");
         int max = scanner.nextInt();
-        System.out.println("хотите ли отсортировать список перед сохранением " +
-                " 1)да " + " 2)нет");
         int[] newList = integerGenerator.generateNumbers(numberOfElements, min, max);
+        System.out.println("хотите ли отсортировать список перед сохранением " + " 1)да " + " 2)нет");
+
         if (scanner.nextInt() == 1) {
             integerGenerator.sort(newList);
-            System.out.println("Список успешно сохранен и отсортирован: " + Arrays.toString(newList));
-        } else {
             fileRepository.saveArray(newList);
-
+            System.out.println("Список успешно сохранен и отсортирован: " + Arrays.toString(newList));
+        }
+        else {
+            fileRepository.saveArray(newList);
             System.out.println("Список успешно сохранен: " + Arrays.toString(newList));
         }
     }
